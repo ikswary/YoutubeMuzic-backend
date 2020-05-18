ac3b2cb677b5f30fd18cff0e516da50f9fd9559f
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-05-18 22:18:01 +0900

    add MainView and Test

--- music/views.py
+++ music/views.py
@@ -1,11 +1,22 @@
 import os
+import json
+import random
 
-from django.http import StreamingHttpResponse
-from django.views import View
+from django.http      import StreamingHttpResponse, HttpResponse, JsonResponse
+from django.views     import View
 from django.shortcuts import get_object_or_404
-from pydub import AudioSegment
+from django.db.models import F
+from pydub            import AudioSegment
 
-from .models import Media
+from .models          import (
+    Collection,
+    Playlist,
+    Media,
+    Artist,
+    Type,
+    Thumbnail,
+    Hotlist
+)
 
 
 class RangeFileWrapper(object):
@@ -47,3 +58,71 @@ class StreamView(View):
                                      status=200, content_type='audio/mp3')
         resp['Cache-Control'] = 'no-cache'
         return resp
+
+class MainView(View):
+
+    def get(self, request):
+
+        range_list = [1,7,13]
+
+        if request.GET :
+            range_list = request.GET.getlist(
+                'collection_id'
+            )
+
+        collection = Collection.objects.prefetch_related(
+            'playlist_set'
+        )
+
+        payload = {
+                'contents':
+                [
+                    {
+                        'collection':collection.filter(id=i).values('name').first()['name'],
+                        'elements':
+                        list(
+                            collection.filter(id=i).annotate(
+                                list_id     = F('playlist__id'),
+                                list_name   = F('playlist__name'),
+                                list_thumb  = F('playlist__thumbnail_id__url'),
+                                list_type   = F('playlist__type_id__name'),
+                                list_artist = F('playlist__artist')
+                            ).values(
+                                'list_id',
+                                'list_name',
+                                'list_thumb',
+                                'list_type',
+                                'list_artist'
+                            )
+                        )
+                    }
+                    for i in range_list
+                ]
+        }
+
+        if not request.GET :
+            payload[
+                'main_thumb'
+            ]=collection.filter(
+                name = payload[
+                    'contents'
+                ][0][
+                    'collection'
+                ]
+            ).values(
+                'thumbnail_id__url'
+            ).first()[
+                'thumbnail_id__url'
+            ]
+            payload[
+                'range_list'
+            ]=random.sample(
+                [3,4,8,10,12,15],
+                6
+            )
+
+        return JsonResponse(
+            payload,
+            status=200
+        )
+
