d40e87fe2334b78191475855f53b838341a0c16e
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-05-18 22:18:01 +0900

    add MainView and Test

--- music/models.py
+++ music/models.py
@@ -1,45 +1,50 @@
 from django.db import models
 
 
+
 class Collection(models.Model):
-    name = models.CharField(max_length=100)
+    name      = models.CharField(max_length=100)
     thumbnail = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
-    type = models.ForeignKey('Type', on_delete=models.PROTECT, null=True)
+    type      = models.ForeignKey('Type', on_delete=models.PROTECT, null=True)
 
     class Meta:
         db_table = 'collections'
 
+
 class Playlist(models.Model):
-    name = models.CharField(max_length=100)
+    name        = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    year = models.CharField(max_length=10)
-    artist = models.CharField(max_length=100)
-    thumbnail = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
-    type = models.ForeignKey('Type', on_delete=models.PROTECT, null=True)
-    collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
+    year        = models.CharField(max_length=10)
+    artist      = models.CharField(max_length=100)
+    thumbnail   = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
+    type        = models.ForeignKey('Type', on_delete=models.PROTECT, null=True)
+    collection  = models.ForeignKey('Collection', on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'playlists'
 
+
 class Media(models.Model):
-    name = models.CharField(max_length=200)
-    length = models.CharField(max_length=20)
-    album = models.CharField(max_length=200)
-    artist = models.ForeignKey('Artist', on_delete=models.PROTECT, null=True)
-    thumbnail = models.ForeignKey('Thumbnail', on_delete=models.PROTECT, null=True)
+    name       = models.CharField(max_length=200)
+    length     = models.CharField(max_length=20)
+    album      = models.CharField(max_length=200)
+    artist     = models.ForeignKey('Artist', on_delete=models.PROTECT, null=True)
+    thumbnail  = models.ForeignKey('Thumbnail', on_delete=models.PROTECT, null=True)
     collection = models.ForeignKey('Collection', on_delete=models.SET_NULL, null=True)
-    playlist = models.ForeignKey('Playlist', on_delete=models.SET_NULL, null=True)
+    playlist   = models.ForeignKey('Playlist', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'medias'
 
+
 class Artist(models.Model):
-    name = models.CharField(max_length=200)
+    name        = models.CharField(max_length=200)
     description = models.CharField(max_length=300, null=True)
-    thumbnail = models.ForeignKey('Thumbnail', on_delete=models.PROTECT, null=True)
+    thumbnail   = models.ForeignKey('Thumbnail', on_delete=models.PROTECT, null=True)
 
     class Meta:
         db_table = 'artists'
+
 
 class Type(models.Model):
     name = models.CharField(max_length=20)
@@ -47,14 +52,20 @@ class Type(models.Model):
     class Meta:
         db_table = 'types'
 
+
 class Thumbnail(models.Model):
     url = models.CharField(max_length=2000)
 
     class Meta:
         db_table = 'thumbnails'
 
+
 class Hotlist(models.Model):
-    title = models.CharField(max_length=50)
-    thumbnail = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
-    artist = models.CharField(max_length=50)
-    views = models.CharField(max_length=10)
+    title       = models.CharField(max_length=50)
+    thumbnail   = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
+    artist      = models.CharField(max_length=50)
+    views       = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'hotlists'
+
