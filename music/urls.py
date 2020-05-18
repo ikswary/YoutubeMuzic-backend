963026e84675093c3f8a80238e789e6378e7d826
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-05-18 22:18:01 +0900

    add MainView and Test

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import StreamView
+from django.urls import path, include
+
+from .views import (
+    StreamView,
+    MainView
+)
 
 urlpatterns = [
-   path('/<int:media_id>', StreamView.as_view()),
+    path('/<int:media_id>',StreamView.as_view()),
+    path('/main',MainView.as_view()),
 ]
