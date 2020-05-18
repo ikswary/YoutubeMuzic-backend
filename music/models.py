16201148875f3036488a5481ec0cdb0f42e65f7a
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-05-18 22:18:01 +0900

    add MainView and Test

--- music/models.py
+++ music/models.py
@@ -1,45 +1,50 @@
 from django.db import models
 
 
+
 class Collection(models.Model):
-    name      = models.CharField(max_length=50)
+    name      = models.CharField(max_length=100)
     thumbnail = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
-    type      = models.ForeignKey('Type', on_delete=models.PROTECT)
+    type      = models.ForeignKey('Type', on_delete=models.PROTECT, null=True)
 
     class Meta:
         db_table = 'collections'
 
+
 class Playlist(models.Model):
-    name       = models.CharField(max_length=40)
-    artist     = models.ForeignKey('Artist', on_delete=models.PROTECT)
-    thumbnail  = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
-    type       = models.ForeignKey('Type', on_delete=models.PROTECT)
-    collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
+    name        = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    year        = models.CharField(max_length=10)
+    artist      = models.CharField(max_length=100)
+    thumbnail   = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
+    type        = models.ForeignKey('Type', on_delete=models.PROTECT, null=True)
+    collection  = models.ForeignKey('Collection', on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'playlists'
 
+
 class Media(models.Model):
-    name       = models.CharField(max_length=100)
-    length     = models.TimeField()
-    views      = models.IntegerField()
-    url        = models.CharField(max_length=300)
-    artist     = models.ForeignKey('Artist', on_delete=models.PROTECT)
-    thumbnail  = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
-    type       = models.ForeignKey('Type', on_delete=models.PROTECT)
-    collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
+    name       = models.CharField(max_length=200)
+    length     = models.CharField(max_length=20)
+    album      = models.CharField(max_length=200)
+    artist     = models.ForeignKey('Artist', on_delete=models.PROTECT, null=True)
+    thumbnail  = models.ForeignKey('Thumbnail', on_delete=models.PROTECT, null=True)
+    collection = models.ForeignKey('Collection', on_delete=models.SET_NULL, null=True)
+    playlist   = models.ForeignKey('Playlist', on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'medias'
 
+
 class Artist(models.Model):
     name        = models.CharField(max_length=200)
     description = models.CharField(max_length=300, null=True)
-    url         = models.CharField(max_length=300)
-    thumbnail   = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
+    thumbnail   = models.ForeignKey('Thumbnail', on_delete=models.PROTECT, null=True)
 
     class Meta:
         db_table = 'artists'
+
 
 class Type(models.Model):
     name = models.CharField(max_length=20)
@@ -47,8 +52,21 @@ class Type(models.Model):
     class Meta:
         db_table = 'types'
 
+
 class Thumbnail(models.Model):
-    url = models.CharFieldField(max_length=300)
+    url = models.CharField(max_length=2000)
 
     class Meta:
         db_table = 'thumbnails'
+
+
+class Hotlist(models.Model):
+    title       = models.CharField(max_length=50)
+    thumbnail   = models.ForeignKey('Thumbnail', on_delete=models.PROTECT)
+    artist      = models.CharField(max_length=50)
+    views       = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'hotlists'
+
+
--- music/tests.py
+++ music/tests.py
@@ -2,14 +2,19 @@ from datetime import time
 
 from django.test import TestCase, Client
 
-from .models import (
-    Artist,
-    Thumbnail,
-    Type,
+from django.db.models import F
+
+from .models          import (
     Collection,
     Playlist,
-    Media
+    Media,
+    Artist,
+    Type,
+    Thumbnail,
+    Hotlist
 )
+
+clnt = Client()
 
 
 class StreamViewTest(TestCase):
@@ -65,3 +70,60 @@ class StreamViewTest(TestCase):
         response = client.get('music/56473')
 
         self.assertEqual(response.status_code, 404)
+
+
+class MainViewTest(TestCase):
+    BASIC_COLLECTION_IDS = [1, 7, 13]
+    FIRST_PAGINATION_IDS = clnt.get('/music/main').json()['range_list'][:3]
+    FIRST_PAGINATION_URL = '/music/main?collection_id={}&collection_id={}&collection_id={}'.format(
+        *FIRST_PAGINATION_IDS)
+
+    def test_first_req_contents(self):
+        self.assertEqual(clnt.get('/music/main').json()['contents'],[{
+            'collection':Collection.objects.filter(id=i).values('name').first()['name'],
+            'elements':list(
+                Collection.objects.prefetch_related('playlist_set').filter(id=i).annotate(
+                    list_id     = F('playlist__id'),
+                    list_name   = F('playlist__name'),
+                    list_thumb  = F('playlist__thumbnail_id__url'),
+                    list_type   = F('playlist__type_id__name'),
+                    list_artist = F('playlist__artist')
+                ).values(
+                    'list_id',
+                    'list_name',
+                    'list_thumb',
+                    'list_type',
+                    'list_artist'))
+        } for i in self.BASIC_COLLECTION_IDS])
+
+    def test_first_req_infra_elements(self):
+        res = clnt.get('/music/main')
+
+        self.assertEqual(
+            res.json()['main_thumb'],
+            Collection.objects.filter(id = 1).values('thumbnail_id__url').first()['thumbnail_id__url']
+        )
+        self.assertTrue(res.json()['range_list'])
+
+    def test_paginate_req_contents_length(self):
+        res = clnt.get(self.FIRST_PAGINATION_URL)
+
+        self.assertEqual(
+            len(res.json()['contents']),
+            3)
+
+    def test_paginate_req_unexpected_keys(self):
+        res = clnt.get(self.FIRST_PAGINATION_URL)
+
+        def for_test():
+            try:
+                res.json()['main_thumb']
+                res.json()['range_list']
+
+                return True
+
+            except KeyError:
+                return False
+
+        self.assertFalse(for_test())
+
